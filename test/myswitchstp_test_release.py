5dbb961bfaa7a5fb318c900a817ff77e03d120da
kennyjin <jianianjin@gmail.com>
2019-03-02 18:20:38 -0600

    Update myswitchstp_test_release.py

--- test/myswitchstp_test_release.py
+++ test/myswitchstp_test_release.py
@@ -3,9 +3,8 @@
 Derived from : jsommers switchyard examples"""
 
 from switchyard.lib.userlib import *
-from spanningtreemessage import SpanningTreeMessage         # change to your naming of spanning tree message
+from spanningtreemessage import SpanningTreeMessage
 from switchyard.lib.userlib import *
-
 
 def mk_stp_pkt(root_id, hops, hwsrc="20:00:00:00:00:01", hwdst="ff:ff:ff:ff:ff:ff"):
     spm = SpanningTreeMessage(root=root_id, hops_to_root=hops)
@@ -17,7 +16,6 @@ def mk_stp_pkt(root_id, hops, hwsrc="20:00:00:00:00:01", hwdst="ff:ff:ff:ff:ff:f
     p = Packet(raw=xbytes)
     return p
 
-
 def mk_pkt(hwsrc, hwdst, ipsrc, ipdst, reply=False):
     ether = Ethernet(src=hwsrc, dst=hwdst, ethertype=EtherType.IP)
     ippkt = IPv4(src=ipsrc, dst=ipdst, protocol=IPProtocol.ICMP, ttl=32)
@@ -28,16 +26,11 @@ def mk_pkt(hwsrc, hwdst, ipsrc, ipdst, reply=False):
         icmppkt.icmptype = ICMPType.EchoRequest
     return ether + ippkt + icmppkt
 
-
 def hub_tests():
     s = TestScenario("Switch Tests")
     s.add_interface('eth0', '20:00:00:00:00:01')
     s.add_interface('eth1', '20:00:00:00:00:02')
     s.add_interface('eth2', '20:00:00:00:00:03')
-
-
-
-
 
     #1. Verify STP packet is flooded out on all ports after initialization.
     stp_pkt = mk_stp_pkt('20:00:00:00:00:01', 0)
@@ -51,41 +44,33 @@ def hub_tests():
     stp_pkt = mk_stp_pkt('10:00:00:00:00:01', 2, hwsrc="30:00:00:00:00:01", hwdst="ff:ff:ff:ff:ff:ff")
     s.expect(PacketInputEvent("eth1", stp_pkt), "Expecting STP packets on eth1: action to be forwarded")
 
-    #5. Verify updated STP packet is flooded out of all ports except input port
+    #5. Verify updated STP packet is flooded out of all ports except input port.
     stp_pkt = mk_stp_pkt('10:00:00:00:00:01', 3, hwsrc="20:00:00:00:00:01", hwdst="ff:ff:ff:ff:ff:ff")
     s.expect(PacketOutputEvent("eth0", stp_pkt, "eth2", stp_pkt), "Expecting STP packets to be broadcasted")
 
-    #6. Receive new stp with same root and less hops
-    stp_pkt = mk_stp_pkt('10:00:00:00:00:01', 0, hwsrc="10:00:00:00:00:01", hwdst="ff:ff:ff:ff:ff:ff")
+    #6. Receive new stp with same root and less hops.
+    stp_pkt = mk_stp_pkt('10:00:00:00:00:01', 1, hwsrc="10:00:00:00:00:01", hwdst="ff:ff:ff:ff:ff:ff")
     s.expect(PacketInputEvent("eth0", stp_pkt), "Expecting STP packets on eth0: action to be forwarded")
 
-    #7. Verify updated STP packet is flooded out of all ports except input port
-    stp_pkt = mk_stp_pkt('10:00:00:00:00:01', 1, hwsrc="20:00:00:00:00:01", hwdst="ff:ff:ff:ff:ff:ff")
+    #7. Verify updated STP packet is flooded out of all ports except input port.
+    stp_pkt = mk_stp_pkt('10:00:00:00:00:01', 2, hwsrc="20:00:00:00:00:01", hwdst="ff:ff:ff:ff:ff:ff")
     s.expect(PacketOutputEvent("eth1", stp_pkt, "eth2", stp_pkt), "Expecting STP packets to be broadcasted")
 
-    # 8. Receive new stp with bigger root
+    # 8. Receive new stp with bigger root.
     stp_pkt = mk_stp_pkt('30:00:00:00:00:01', 0, hwsrc="30:00:00:00:00:01", hwdst="ff:ff:ff:ff:ff:ff")
     s.expect(PacketInputEvent("eth1", stp_pkt), "Expecting STP packets on eth1: action to be discarded")
 
+    #6. Receive new stp with same root and same hops. The port should go in to blocking mode.
+    stp_pkt = mk_stp_pkt('10:00:00:00:00:01', 1, hwsrc="10:00:00:00:00:01", hwdst="ff:ff:ff:ff:ff:ff")
+    s.expect(PacketInputEvent("eth1", stp_pkt), "Expecting STP packets on eth1 with same root and hops")
     # ------------------------------------------------------------------------------------------------------------
 
-    #12., 11.  A normal packet with destination not learnt should be sent out of ports eth0
+    #12., 11.  A normal packet with destination not learnt should be sent out of ports eth0.
     reqpkt = mk_pkt("60:00:00:00:00:01", "70:00:00:00:00:01", '192.168.1.100', '172.16.42.2')
     s.expect(PacketInputEvent("eth2", reqpkt, display=Ethernet),
              "An Ethernet frame from 60:00:00:00:00:00 to 70:00:00:00:00:01 should arrive on eth2")
     s.expect(PacketOutputEvent("eth0", reqpkt, display=Ethernet),
              "Ethernet frame destined for 70:00:00:00:00:01 should be flooded out eth0")
-
-
-
-
-    #16, 15,  A normal packet with destination  learnt should be sent out of ports eth0
-    reqpkt = mk_pkt("60:00:00:00:00:01", "70:00:00:00:00:01", '192.168.1.100', '172.16.42.2')
-    s.expect(PacketInputEvent("eth2", reqpkt, display=Ethernet),
-             "An Ethernet frame from 60:00:00:00:00:00 to 70:00:00:00:00:01 should arrive on eth0")
-    s.expect(PacketOutputEvent("eth0", reqpkt, display=Ethernet),
-             "Ethernet frame destined for 70:00:00:00:00:01 should be flooded out eth0")
-
 
     return s
 
